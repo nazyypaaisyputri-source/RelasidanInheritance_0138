322fe2fafab65b55cd92e164730e16ddebd50dd0
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-05-12 20:00:24 +0700

    implementasi logika tambahPasien dan cetak secara detail.

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -38,6 +38,28 @@ void pasien::tambahDokter(dokter* pDokter) {
 }
 
 void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { // auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diitera
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+int main() {
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
     
     return 0;
 }
