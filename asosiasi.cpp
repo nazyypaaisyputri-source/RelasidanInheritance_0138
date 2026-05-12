32d94a5a528cf7e404468a3fd16b4ead5797c71f
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-05-12 19:58:49 +0700

    mefinisi class, constructor, destructor, dan prototype method.

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -3,6 +3,41 @@
 using namespace std;
 
 class dokter;
+class pasien { // asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
 
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
 
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    
+    return 0;
 }
