56ac68d6e7f1105515c3d5cb34cf84c2d911d011
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-05-12 18:03:51 +0700

    implementasi constructor dan destructor pada class anak

--- anak.h
+++ anak.h
@@ -3,5 +3,14 @@
 #include <iostream>
 using namespace std;
 
-
+class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
 #endif
