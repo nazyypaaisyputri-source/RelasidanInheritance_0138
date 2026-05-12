a58831e0b67e58ea102ded44ba5dfb476b865240
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-05-12 20:07:53 +0700

    membuat objek baru

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -60,6 +60,9 @@ void dokter::cetakPasien() {
 int main() {
     dokter* varDokter1 = new dokter("dr.Budi");
     dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
     
     return 0;
 }
